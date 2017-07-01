822ac3fac4a796ab5fe46a2e3a77361d9588d4a9
Nick <nickmour5@gmail.com>
2017-07-01 22:12:51 +0300

    Fixed dir problems with the scripts

---
+++ ctm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Your last theme was :"
+cat log.txt
+truncate -s 0 log.txt
+echo "The NEW themes must greater than 20 or u will dl duplicates!!"
+echo "Procced ? (Type y for yes)"
+read a
+if [ $a == y ]; then
+echo "What the last page u wish to dl?"
+read x
+for (( i=1; i<=$x; i++ )); do
+
+  wget http://psv.altervista.org/index.php?p=$i
+  grep -Po '(?<=href=")[^"]*' index.php\?p\=$i | grep 'theme.php?id=' | cut -d "=" -f 2 >> id.txt
+  awk '{print "http://psv.altervista.org/download.php?id="$0}' id.txt >> theme.txt
+  rm index.php\?p\=$i
+done
+mkdir themes && cd themes
+wget --content-disposition -i ../theme.txt
+cd ..
+id=$(head -n 1 id.txt)
+wget http://psv.altervista.org/theme.php?id=$id
+sed -n 's/.*<h2 style="margin:5px 0 0 0;">\(.*\)<\/h2>.*/\1/p' theme.php\?id\=$id >> log.txt
+rm id.txt theme.txt theme.php\?id\=$id
+else
+  echo "EXITING..."
+  exit
+fi
--- vstema.sh
+++ vstema.sh
@@ -3,13 +3,13 @@ echo "Your last theme was :"
 cat logt.txt
 truncate -s 0 logt.txt
 echo "The NEW themes must greater than 9 or u will dl duplicates!!"
-echo "Procced ?"
+echo "NOTE: the script will remove the tema folder with the previous themes,BACKUP THEM!"
+echo "Procced ? (Type y for yes)"
 read a
 if [ $a == y ]; then
 echo "What the last page u wish to dl?"
 read x
 for (( i=1; i<=$x; i++ )); do
-
   wget http://vstema.com/livearea?page=$i
   grep -Po '(?<=href=")[^"]*' livearea\?page\=$i | grep '/themes/' | awk '{print "http://vstema.com"$0}' >> theme.txt
    if [ $i == 1 ]; then
@@ -17,12 +17,18 @@ for (( i=1; i<=$x; i++ )); do
    fi
   rm livearea\?page\=$i
 done
-mkdir tema && cd tema
-wget -i --content-disposition theme.txt
+rm -f -r tema/
+mkdir tema
+cd tema/
+wget -i ../theme.txt
+for f in *.zip; do
+mv "$f" "${f#??????????}"
+done
 cd ..
 wget -O thm $log
 sed -n 137p thm >> logt.txt
 rm theme.txt thm
 else
+  echo "EXITING..."
   exit
 fi
